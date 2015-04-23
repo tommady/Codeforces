8002602b1e0f317021079f057aecb53fc0a7a558
lindroos <emptysmile1013@gmail.com>
2015-04-23 10:47:22 +0800

    little changes on 490B

--- 490/B. Queue/main.c
+++ 490/B. Queue/main.c
@@ -6,8 +6,6 @@ typedef struct data
     int id;
     int front;
     int behind;
-    struct data *prev;
-    struct data *next;
     
 } data_l;
 
@@ -17,13 +15,23 @@ int main(void)
 {
     int n, i;
     scanf("%d", &n);
-    data_l line_;
+    data_l input_line[n];
     data_l sorted_line[n];
     
     for(i=0; i<n; i++)
     {
         scanf("%d %d", line[i].front, line[i].behind);
-        if(line[i].front == 0)
+        
+        if(input_line[i].front == 0)
+        {
+        	// second can be identify
+        	input_line[1].id = input_line[i].behind;
+        }
+        if(input_line[i].behind == 0)
+        {
+        	// penultimate can be identify
+        	input_line[n-1].id = input_line[i].front;
+        }
     }
     
     return 0;

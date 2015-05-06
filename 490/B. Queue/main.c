5fbe7effa64b5cceff09c2f5af8a8402bad62c22
lindroos <emptysmile1013@gmail.com>
2015-05-06 09:11:37 +0800

    after all I choose normal array for fast mapping, Accepted

--- 490/B. Queue/main.c
+++ 490/B. Queue/main.c
@@ -1,127 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct data
-{
-    int id;
-    int front;
-    int behind;
-    struct data *next;
-    
-} data_l;
-
-data_l *init_link(int front, int behind, int id);
-void add_link(data_l *head, data_l *new_link);
-void pop_link(data_l *link);
-void delet_all(data_l *head);
+#define MAX_SIZE 1000001
 
 int main(void)
 {
-    int n, i;
+    int n, i, odd_start, even_start;
+    int list[MAX_SIZE] = {0};
+    int counter_list[MAX_SIZE] = {0};
+    
     scanf("%d", &n);
-
-    data_l sorted_line[n];
-    
-    // initialize sorted_line with id = -1;
-    for(i=0; i<n; i++)
-    {
-    	sorted_line[i].id = -1;
-    }
-    
-    // need to be found list
-    data_l *ntbf_list = init_link(-1, -1, -1);
     
     for(i=0; i<n; i++)
     {
     	int front, behind;
         scanf("%d %d", &front, &behind);
         
-        if(front == 0)
-        {
-        	// second can be identify
-        	sorted_line[1].id = behind;
-        }
-        else if(behind == 0)
-        {
-        	// penultimate can be identify
-        	sorted_line[n-2].id = front;
-        }
-        else
-        {
-        	add_link(ntbf_list, init_link(front, behind, 0));
-        }
+        list[front] = behind;
+        counter_list[front]++;
+        counter_list[behind]--;
     }
     
-    // looping the need to be found list for matching id.
-    // TODO: change sorted_line to double linked list and change the search logic
-    data_l *tmp = ntbf_list->next;
-    while(tmp != NULL)
+    odd_start = 0;
+    even_start = list[0];
+    
+    while(1)
     {
-    	for(i=0; i<n; i++)
-    	{
-    		if(sorted_line[i].id == -1)
-    		{
-				if(tmp->front == sorted_line[i].id)
-				{
-					sorted_line[i+1].id = tmp->behind;
-					pop_link(tmp);
-				}
-				else if(tmp->behind == sorted_line[i].id)
-				{
-					sorted_line[i-1].id = tmp->front;
-					pop_link(tmp);
-				}
-    		}
+        if(counter_list[odd_start] == 1)
+            break;
+        odd_start++;
+    }
+    
+    for(i=1; i<=n; i++)
+    {
+        if(i & 1)
+        {
+    	    printf("%d ", odd_start);
+    	    odd_start = list[odd_start];
     	}
-    	
-    	tmp = tmp->next;
-    }
-    
-    for(i=0; i<n; i++)
-    {
-    	printf("%d ", sorted_line[i].id);
+    	else
+    	{
+    	    printf("%d ", even_start);
+    	    even_start = list[even_start];
+    	}
     }
     printf("\n");
     
-    delet_all(ntbf_list);
-    
     return 0;
-}
-
-data_l *init_link(int front, int behind, int id)
-{
-	data_l *new_link = calloc(1, sizeof(data_l));
-	new_link->id = id;
-	new_link->front = front;
-	new_link->behind = behind;
-	new_link->next = NULL;
-	
-	return new_link;
-}
-
-void add_link(data_l *head, data_l *new_link)
-{
-	data_l *tmp = head->next;
-	head->next = new_link;
-	new_link->next = tmp;
-}
-
-void pop_link(data_l *link)
-{
-	// not really pop the link, but change the value to let the loop void search.
-	link->id = -1;
-	link->front = -1;
-	link->behind = -1;
-}
-
-void delet_all(data_l *head)
-{
-	data_l *tmp;
-	
-	while(head != NULL)
-	{
-		tmp = head;
-		head = head->next;
-		free(tmp);
-	}
 }
